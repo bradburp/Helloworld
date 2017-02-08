b7b71a43a6083c3a0dc7a87bf7d447be87af0e13
Paul Bradbury <paul.bradbury@live.co.uk>
2017-02-08 13:17:24 +0000

    Initial commit

---
+++ src/Helloworld.java
@@ -0,0 +1,21 @@
+/**
+ * Created by paul on 08/02/2017.
+ */
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Paul!");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int mydiff = 1000 - myTotal;
+
+        System.out.println(myFirstNumber);
+        System.out.println("myFirstNumber");
+        System.out.println(myTotal);
+        System.out.println(mydiff);
+    }
+}
